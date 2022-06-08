df05ced01fc16b65d43c856b3228d92979828f7d
Sidorov Peter <kripns06@gmail.com>
2022-06-08 12:45:44 +0300

    feature: implement card adding popup

--- script/script.js
+++ script/script.js
@@ -1,16 +1,14 @@
 
 // нажимаем на эдит буттон, открываем попап
 // заполняем вэлъю инпутов
-const editButton = document.querySelector('.edit-button');
-const popup = document.querySelector('.popup');
+const profilePopup = document.querySelector('.popup_type_profile');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__heading');
 const profileJob = document.querySelector('.profile__subheading');
 
-const openPopup = () => popup.classList.add('popup_opened'); // фн открывает попап
-editButton.addEventListener('click', () => {
-  openPopup();
+document.querySelector('.edit-button').addEventListener('click', () => {
+  openPopup(profilePopup);
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 });
@@ -18,23 +16,68 @@ editButton.addEventListener('click', () => {
 
 // нажимаем на крестик, закрываем попап.
 const popupCloseIcon = document.querySelector('.popup__close-icon');
-const closePopup = () => popup.classList.remove('popup_opened'); //фн закрывает попап
 
-popupCloseIcon.addEventListener('click', closePopup);
+profilePopup.querySelector('.popup__close-icon')
+.addEventListener('click', () => closePopup(profilePopup));
 
 
 // обработчик отправки формы
-// отменяем стандартную отправку
-// заполняем инпуты
-// закрываем по нажатию на субмит
-const popupForm = document.querySelector('.popup__form');
-
-const formSubmitHandler = (event) => {
-  event.preventDefault();
-  profileName.textContent = `${nameInput.value}`;
+function profileFormSubmitHandler (event) {
+  event.preventDefault();                               // отменяем стандартную отправку
+  profileName.textContent = `${nameInput.value}`;       // заполняем инпуты
   profileJob.textContent = `${jobInput.value}`;
-  closePopup();
-}
+  closePopup(profilePopup);                             // закрываем по нажатию на субмит
+};
 
-// отслеживаем субмит
-popupForm.addEventListener('submit', formSubmitHandler);
+
+document.getElementsByName('edit-profile')[0]           //находим форму изменения профайла
+.addEventListener('submit', profileFormSubmitHandler);  // отслеживаем субмит
+
+
+// Открываем попап добавления карточки
+const cardAddingPopup = document.querySelector('.popup_type_card-adding');
+
+document.querySelector('.add-card-button').addEventListener('click', () => {
+  openPopup(cardAddingPopup);
+});
+
+//Закрываем попап на крестик
+cardAddingPopup.querySelector('.popup__close-icon')
+.addEventListener('click', () => closePopup(cardAddingPopup));
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function openPopup (popupName) {
+  popupName.classList.add('popup_opened')
+}; // фн открывает попап
+
+function closePopup (popupName) {
+  popupName.classList.remove('popup_opened')
+}; //фн закрывает попап
